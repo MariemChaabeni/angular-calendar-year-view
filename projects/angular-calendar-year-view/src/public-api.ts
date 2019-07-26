84d1218254de049192e737c843d1157b520cde78
Mike <kang.mike@gmail.com>
2019-07-26 04:40:12 -0600

    added support for library deployment and publishing
    updated schematics to use SASS
    
    updated readme

---
+++ projects/angular-calendar-year-view/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-calendar-year-view
+ */
+
+export * from './lib/angular-calendar-year-view.component';
+export * from './lib/angular-calendar-year-view.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule, } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
-import { AngularCalendarYearViewComponent } from './angular-calendar-year-view/angular-calendar-year-view.component';
+import { AngularCalendarYearViewModule } from 'projects/angular-calendar-year-view/src/public-api';
 import { AppComponent } from './app.component';
-import {PopoverModule} from 'ngx-bootstrap/popover'
+
 @NgModule({
   declarations: [
-    AppComponent,
-    AngularCalendarYearViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule,
-    PopoverModule.forRoot()
+    AngularCalendarYearViewModule.forRoot()
   ],
   providers: [],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
